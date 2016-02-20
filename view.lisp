8fb27c5e7195cde42c3c6ed7c15a4f6c841100f4
Eitaro Fukamachi <e.arrows@gmail.com>
2016-02-21 04:30:14 +0900

    Add render-json.

--- view.lisp
+++ view.lisp
@@ -4,17 +4,22 @@
   (:import-from #:utopian/app
                 #:*session*)
   (:import-from #:lack.middleware.csrf)
+  (:import-from #:jonathan)
   (:import-from #:djula
                 #:add-template-directory
                 #:compile-template*
                 #:render-template*
                 #:def-tag-compiler)
-  (:export #:render))
+  (:export #:render
+           #:render-json))
 (in-package #:utopian/view)
 
 (defun render (template-path &optional env)
   (let ((template (djula:compile-template* (princ-to-string template-path))))
     (apply #'djula:render-template* template nil env)))
+
+(defun render-json (object)
+  (jojo:to-json object))
 
 (djula::def-tag-compiler csrf-token ()
   (lambda (stream)
