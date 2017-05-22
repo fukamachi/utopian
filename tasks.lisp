59104a31573bc1fb7ca40c7e60aa3133f3f196ba
Eitaro Fukamachi <e.arrows@gmail.com>
2017-05-22 16:02:01 +0900

    Stop run 'gulp watch' when starting 'lake server'.
    It should be replaced with Webpack2 in the future.

--- tasks.lisp
+++ tasks.lisp
@@ -12,13 +12,6 @@
   (:export #:load-tasks))
 (in-package #:utopian/tasks)
 
-(defun run-gulp-watch ()
-  (uiop/run-program::%process-info-pid
-   (uiop/run-program::%run-program '("node_modules/.bin/gulp" "watch")
-                                   :output *standard-output*
-                                   :error-output *error-output*
-                                   :wait nil)))
-
 (defun connect-to-db ()
   (apply #'mito:connect-toplevel (connection-settings :maindb)))
 
@@ -40,15 +33,9 @@
   (task "default" ("server"))
 
   (task "server" ()
-    (let (pid)
-      (unless (productionp)
-        (utopian/watcher:start-watching)
-        (setf pid (run-gulp-watch)))
-      (clack:clackup (project-path #P"app.lisp")
-                     :use-thread nil
-                     :debug (not (productionp)))
-      (when pid
-        (uiop:run-program `("kill" ,(write-to-string pid))))))
+    (clack:clackup (project-path #P"app.lisp")
+                   :use-thread nil
+                   :debug (not (productionp))))
 
   (namespace "db"
     (task "migrate" ()
