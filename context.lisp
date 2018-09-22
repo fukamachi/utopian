6df4ba08b7b80ae78acf8b1029563ef2807563c6
Eitaro Fukamachi <e.arrows@gmail.com>
2018-09-23 00:25:15 +0900

    Reexport from lack, lack.request & lack.response.

--- context.lisp
+++ context.lisp
@@ -1,6 +1,15 @@
-(defpackage #:utopian/context
+(uiop:define-package #:utopian/context
   (:use #:cl)
-  (:export #:*request*
+  (:use-reexport #:lack
+                 #:lack.response
+                 #:lack.request
+                 #:lack.component)
+  (:import-from #:lack.component
+                #:to-app
+                #:call)
+  (:export #:to-app
+           #:call
+           #:*request*
            #:*response*))
 (in-package #:utopian/context)
 
