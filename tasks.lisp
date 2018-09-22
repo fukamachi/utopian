18a227a3fa8ccb2a864144994b1eaf6dd69c4bb0
Eitaro Fukamachi <e.arrows@gmail.com>
2018-09-23 00:48:24 +0900

    Fix tasks.

--- tasks.lisp
+++ tasks.lisp
@@ -9,23 +9,31 @@
                 #:clackup)
   (:export #:db-connect
            #:migrate
+           #:migration-status
            #:generate-migrations
            #:server))
 (in-package #:utopian/tasks)
 
 (defun db-connect (app)
-  (with-config (app)
+  (with-config ((if (pathnamep app)
+                    (clack::eval-file app)
+                    app))
     (let ((config (config :database)))
       (unless config
         (error "No database settings. (APP_ENV=~A)" (appenv)))
       (apply #'mito:connect-toplevel config))))
 
-(defun migrate (app)
+(defun migrate (app &key dry-run)
   (db-connect app)
-  (mito:migrate #P"db/"))
+  (mito:migrate #P"db/" :dry-run dry-run))
+
+(defun migration-status (app)
+  (db-connect app)
+  (mito:migration-status #P"db/"))
 
 (defun generate-migrations (app)
   (db-connect app)
+  ;; TODO: load models
   (mito:generate-migrations #P"db/"))
 
 (defun server (app)
