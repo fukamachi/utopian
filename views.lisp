01a74a60530e2b7db5a5468a78a9a03721e5ee98
Eitaro Fukamachi <e.arrows@gmail.com>
2019-01-26 05:33:40 +0900

    Update utopian/view to work with the latest LSX.

--- views.lisp
+++ views.lisp
@@ -3,17 +3,19 @@
   (:import-from #:utopian/context
                 #:*response*
                 #:response-headers)
-  (:import-from #:utopian/file-loader
-                #:intern-rule)
-  (:import-from #:lsx)
+  (:import-from #:lsx
+                #:render)
   (:import-from #:closer-mop)
-  (:export #:render
-           #:defview))
+  (:export #:defview
+           ;; Reexport from LSX
+           #:render))
 (in-package #:utopian/views)
 
 (defvar *default-content-type* "text/html")
 
-(defclass utopian-view-class (lsx:component-class)
+(defclass utopian-view (lsx:template) ())
+
+(defclass utopian-view-class (lsx:template-class)
   ((content-type :initarg :content-type)
    (auto-escape :initarg :auto-escape
                 :initform '(t))))
@@ -24,7 +26,7 @@
       *default-content-type*))
 
 (defmacro defview (name superclasses slots &rest options)
-  `(defclass ,name (,@superclasses lsx:component)
+  `(defclass ,name (,@superclasses utopian-view)
      ,slots
      (:metaclass utopian-view-class)
      ,@options))
@@ -32,12 +34,11 @@
 (defmethod c2mop:validate-superclass ((class utopian-view-class) (super standard-class))
   t)
 
-(defun render (view-class &rest view-args)
-  (let* ((view (apply #'make-instance view-class view-args))
-         (view-class (class-of view))
+(defmethod lsx:render-object ((view utopian-view) stream)
+  (let* ((view-class (class-of view))
          (content-type (view-content-type view-class)))
     (when content-type
       (setf (getf (response-headers *response*) :content-type)
             content-type))
     (let ((lsx:*auto-escape* (first (slot-value view-class 'auto-escape))))
-      (lsx:render-object view nil))))
+      (call-next-method))))
